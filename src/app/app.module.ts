75277becc5ad399ca0881f91ee81ae2abf4913d1
kirezibana <hortensebana32@gmail.com>
2020-11-07 17:32:25 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitNavbarComponent } from './git-navbar/git-navbar.component';
+import { GithomeComponent } from './githome/githome.component';
+import { GitFormComponent } from './git-form/git-form.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { RepositoriesComponent } from './repositories/repositories.component';
+// import { GithubComponent } from './github/github.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitNavbarComponent,
+    GithomeComponent,
+    GitFormComponent,
+    HighlightDirective,
+    DateCountPipe,
+    RepositoriesComponent,
+    // GithubComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
